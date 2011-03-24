dd28325a52e466be78e573da9a2d511e0383c517
mmozuras <mindaugas.mozuras@gmail.com>
2011-03-24 14:14:20 +0200

    Fixed path aliases

--- Aliases.ps1
+++ Aliases.ps1
@@ -1,3 +1,4 @@
 function .. {Set-Location .. }
-function ... {Set-Location ... }
-function .... {Set-Location .... }
+function ... {Set-Location ../.. }
+function .... {Set-Location ../../.. }
+function ..... {Set-Location ../../../.. }
