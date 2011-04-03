5802c9b03230fc78b124d6182d91ce0689b53b54
Mindaugas Mozuras <mindaugas.mozuras@gmail.com>
2011-04-03 15:25:04 +0300

    Added a couple of svn/git  aliases

--- GitAliases.ps1
+++ GitAliases.ps1
@@ -1,6 +1,7 @@
 Set-Alias g git
 function Git-Status { git status }
 Set-Alias gs Git-Status
+Set-Alias gst Git-Status
 function Git-Pull { git pull }
 Set-Alias gpl Git-Pull
 function Git-Push { git push }
--- SvnAliases.ps1
+++ SvnAliases.ps1
@@ -1,1 +1,6 @@
 Set-Alias s svn
+function Svn-Status { svn status }
+Set-Alias ss Svn-Status
+Set-Alias sst Svn-Status
+function Svn-Update { svn update }
+Set-Alias su Svn-Update
